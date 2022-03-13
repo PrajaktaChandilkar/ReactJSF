0af49f61fbab5fb76ae412cd6957f63f11bffaaf
PrajaktaChandilkar <prajakta0704@gmail.com>
2022-03-14 00:51:32 +0530

    Birthday-reminder

---
+++ birthday-reminder/src/Components/Birthday.js
@@ -0,0 +1,22 @@
+import React,{ Component } from 'react'
+import Data from './../Resources/Data'
+
+class Birthday extends Component{
+    constructor(props){
+        super(props){
+            this.state = {
+                people:Data
+            }
+        }
+    }
+    render(){
+        return(
+            <main>
+                <section className='container'>
+
+                </section>
+            </main>
+        )
+    }
+
+}
