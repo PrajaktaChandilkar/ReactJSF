b7e2b3445cf07d530fdecdfa84b4b3d085baabca
PrajaktaChandilkar <prajakta0704@gmail.com>
2022-04-01 11:19:06 +0530

    menu

---
+++ menu-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import './App.css'
+
+function App(){
+    return(
+        <>
+        <div className="container">hiipppi</div>
+        <div ><h1>okk<h2>nopjjjjj8jjjjjjjjjjjjjjjjj</h2></h1></div>
+        </>
+    )
+}
+
+export default App
